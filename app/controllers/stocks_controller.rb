47c83b9639d2cf50b07235d5193f2ea74f2833ed
Ahmed Soliman <ahmed.soliman@programmer.net>
2023-03-12 10:46:37 -0500

    handling missing symbol ticker case

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -1,6 +1,12 @@
 class StocksController < ApplicationController
   def search
-    @stock = Stock.new_lookup(params[:stock])
+    symbol_param = params[:stock]
+    unless symbol_param.present?
+      flash.now[:alert] = 'Please enter a symbol to search'
+      render 'users/portfolio', status: :bad_request
+      return
+    end
+    @stock = Stock.new_lookup(symbol_param)
     render 'users/portfolio'
   end
 end
