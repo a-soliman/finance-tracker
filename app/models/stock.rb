0e61874aead63b900def446f23450a0fdc7afd6c
Ahmed Soliman <ahmed.soliman@programmer.net>
2023-03-12 10:33:14 -0500

    added search stock functionality

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,5 +1,8 @@
 class Stock < ApplicationRecord
   def self.new_lookup(ticker_symbol)
-    Polygon.get_closing_price(ticker_symbol)
+    price = Polygon.get_closing_price(ticker_symbol)
+    name = Polygon.get_company_name(ticker_symbol)
+
+    new(ticker: ticker_symbol, name: name, price: price)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root "welcome#index"
+  root 'welcome#index'
+  get 'portfolio', to: 'users#portfolio'
+  get 'search_stock', to: 'stocks#search'
 end
