8c19ed22943d2ba31403cdeda179f41ce6646c1e
Ahmed Soliman <ahmed.soliman@programmer.net>
2023-03-12 10:32:48 -0500

    added Polygon client methods to return the company name

--- app/clients/polygon.rb
+++ app/clients/polygon.rb
@@ -1,16 +1,27 @@
 require 'httparty'
 
+API_KEY = Rails.application.credentials.polygon[:api_key]
+
+# Polygon, an interface that works with the Polygon platform
 class Polygon
-  def self.get_closing_price(ticker_symbol)
-    api_key = Rails.application.credentials.polygon[:api_key]
-    req_url = "https://api.polygon.io/v2/aggs/ticker/#{ticker_symbol}/prev?adjusted=true&apiKey=#{api_key}"
-    
+  def self.get_company_name(ticker_symbol)
+    req_url = "https://api.polygon.io/v3/reference/tickers/#{ticker_symbol}?apiKey=#{API_KEY}"
     headers = {
-      "Content-Type": "application/json"
+      'Content-Type': 'application/json'
     }
     response = HTTParty.get(req_url, headers: headers, format: :plain)
     json = JSON.parse response
-    closing_price = json["results"][0]["c"]
-    closing_price
+
+    json['results']['name']
+  end
+
+  def self.get_closing_price(ticker_symbol)
+    req_url = "https://api.polygon.io/v2/aggs/ticker/#{ticker_symbol}/prev?adjusted=true&apiKey=#{API_KEY}"
+    headers = {
+      'Content-Type': 'application/json'
+    }
+    response = HTTParty.get(req_url, headers: headers, format: :plain)
+    json = JSON.parse response
+    json['results'][0]['c'] # closing price
   end
 end
