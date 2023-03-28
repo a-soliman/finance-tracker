1e995d98830bde034fada38919a848ef7e2d122a
Ahmed Soliman <ahmed@famous.co>
2023-03-28 18:55:24 -0500

    better handling error cases

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -2,8 +2,8 @@ class StocksController < ApplicationController
   def search
     symbol_param = params[:stock]
     unless symbol_param.present?
-      flash.now[:alert] = 'Please enter a symbol to search'
-      render 'users/portfolio', status: :bad_request
+      flash[:alert] = 'Please enter a symbol to search'
+      redirect_to portfolio_path, status: :bad_request
       return
     end
     @stock = Stock.new_lookup(symbol_param)
@@ -12,8 +12,8 @@ class StocksController < ApplicationController
         format.turbo_stream { render turbo_stream: turbo_stream.replace('results', partial: 'users/result', locals: { stock: @stock }) }
       end
     else
-      flash.now[:alert] = 'Please enter a valid symbol to search'
-      render 'users/portfolio', status: :bad_request
+      flash[:alert] = 'Please enter a valid symbol to search'
+      redirect_to portfolio_path, status: :bad_request
     end
   end
 end
