70db0c3e9607f0d79ea36a10fcac57055d15adab
Ahmed Soliman <ahmed@famous.co>
2023-03-30 16:42:09 -0500

    added first and last name columns to users

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,6 @@
 module UsersHelper
+  def full_name
+    return "#{current_user.first_name} #{current_user.last_name}" if current_user.first_name || current_user.last_name
+    "Anonymous"
+  end
 end
