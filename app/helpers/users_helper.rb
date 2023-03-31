a1d7e00e745085715dd606b45e2e4bcc341dc3c1
Ahmed Soliman <ahmed.soliman@programmer.net>
2023-03-31 10:49:34 -0500

    listing friends functionality added

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,6 +1,6 @@
 module UsersHelper
-  def full_name
-    return "#{current_user.first_name} #{current_user.last_name}" if current_user.first_name || current_user.last_name
+  def full_name(user = current_user)
+    return "#{user.first_name} #{user.last_name}" if user.first_name || user.last_name
     "Anonymous"
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :user_stocks, only: [:create, :destroy]
+  resources :friendships, only: [:index, :create, :destory]
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
